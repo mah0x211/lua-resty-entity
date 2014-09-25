9b3a9e36e1063ad3e58594a72a0632cb4c15d680
Masatoshi Teruya <mah0x211@gmail.com>
2014-09-25 15:57:10 +0900

    change: constants.toReadable function has been added.

--- lib/constants.lua
+++ lib/constants.lua
@@ -46,98 +46,111 @@ local HTTP_METHOD = {
 };
 local HTTP_STATUS = {
     -- 1xx infromational
-    CONTINUE                            = 100,
-    SWITCHING_PROTOCOLS                 = 101,
+    ['Continue']                            = 100,
+    ['Switching Protocols']                 = 101,
     -- WebDAV
-    PROCESSING                          = 102,  -- WebDAV; RFC 2518
+    ['Processing']                          = 102,  -- WebDAV; RFC 2518
     -- /WebDAV
     
     -- 2xx successful
-    OK                                  = 200,
-    CREATED                             = 201,
-    ACCEPTED                            = 202,
-    NON_AUTHORITATIVE_INFORMATION       = 203,  -- since HTTP/1.1
-    NO_CONTENT                          = 204,
-    RESET_CONTENT                       = 205,
-    PARTIAL_CONTENT                     = 206,
+    ['OK']                                  = 200,
+    ['Created']                             = 201,
+    ['Accepted']                            = 202,
+    ['Non-Authoritative Information']       = 203,  -- since HTTP/1.1
+    ['No Content']                          = 204,
+    ['Reset Content']                       = 205,
+    ['Partial Content']                     = 206,
     -- WebDAV
-    MULTI_STATUS                        = 207,  -- WebDAV; RFC 4918
-    ALREADY_REPORTED                    = 208,  -- WebDAV; RFC 5842
+    ['Multi-Status']                        = 207,  -- WebDAV; RFC 4918
+    ['Already Reported']                    = 208,  -- WebDAV; RFC 5842
     -- /WebDAV
     
     -- 3xx redirect
-    MULTIPLE_CHOICES                    = 300,
-    MOVED_PERMANENTLY                   = 301,
-    FOUND                               = 302,
-    SEE_OTHER                           = 303,  -- since HTTP/1.1
-    NOT_MODIFIED                        = 304,
-    USE_PROXY                           = 305,  -- since HTTP/1.1
-    TEMPORARY_REDIRECT                  = 307,  -- since HTTP/1.1
+    ['Multiple Choices']                    = 300,
+    ['Moved Permanently']                   = 301,
+    ['Found']                               = 302,
+    ['See Other']                           = 303,  -- since HTTP/1.1
+    ['Not Modified']                        = 304,
+    ['Use Proxy']                           = 305,  -- since HTTP/1.1
+    ['Temporary Redirect']                  = 307,  -- since HTTP/1.1
     
     -- 4xx client error
-    BAD_REQUEST                         = 400,
-    UNAUTHORIZED                        = 401,
-    PAYMENT_REQUIRED                    = 402,
-    FORBIDDEN                           = 403,
-    NOT_FOUND                           = 404,
-    METHOD_NOT_ALLOWED                  = 405,
-    NOT_ACCEPTABLE                      = 406,
-    PROXY_AUTHENTICATION_REQUIRED       = 407,
-    REQUEST_TIMEOUT                     = 408,
-    CONFLICT                            = 409,
-    GONE                                = 410,
-    LENGTH_REQUIRED                     = 411,
-    PRECONDITION_FAILED                 = 412,
-    REQUEST_ENTITY_TOO_LARGE            = 413,
-    REQUEST_URI_TOO_LONG                = 414,
-    UNSUPPORTED_MEDIA_TYPE              = 415,
-    REQUESTED_RANGE_NOT_SATISFIABLE     = 416,
-    EXPECTATION_FAILED                  = 417,
+    ['Bad Request']                         = 400,
+    ['Unauthorized']                        = 401,
+    ['Payment Required']                    = 402,
+    ['Forbidden']                           = 403,
+    ['Not Found']                           = 404,
+    ['Method Not Allowed']                  = 405,
+    ['Not Acceptable']                      = 406,
+    ['Proxy Authentication Required']       = 407,
+    ['Request Timeout']                     = 408,
+    ['Conflict']                            = 409,
+    ['Gone']                                = 410,
+    ['Length Required']                     = 411,
+    ['Precondition Failed']                 = 412,
+    ['Request Entity Too Large']            = 413,
+    ['Request-URI Too Long']                = 414,
+    ['Unsupported Media Type']              = 415,
+    ['Requested Range Not Satisfiable']     = 416,
+    ['Expectation Failed']                  = 417,
     -- WebDAV
-    UNPROCESSABLE_ENTITY                = 422,  -- WebDAV; RFC 4918
-    LOCKED                              = 423,  -- WebDAV; RFC 4918
-    FAILED_DEPENDENCY                   = 424,  -- WebDAV; RFC 4918
+    ['Unprocessable Entity']                = 422,  -- WebDAV; RFC 4918
+    ['Locked']                              = 423,  -- WebDAV; RFC 4918
+    ['Failed Dependency']                   = 424,  -- WebDAV; RFC 4918
     -- /WebDAV
-    UPGRADE_REQUIRED                    = 426,  -- RFC 2817
-    PRECONDITION_REQUIRED               = 428,  -- RFC 6585
-    TOO_MANY_REQUESTS                   = 429,  -- RFC 6585
-    REQUEST_HEADER_FIELDS_TOO_LARGE     = 431,  -- RFC 6585
+    ['Upgrade Required']                    = 426,  -- RFC 2817
+    ['Precondition Required']               = 428,  -- RFC 6585
+    ['Too Many Requests']                   = 429,  -- RFC 6585
+    ['Request Header Fields Too Large']     = 431,  -- RFC 6585
     
     -- 5xx server error
-    INTERNAL_SERVER_ERROR               = 500,
-    NOT_IMPLEMENTED                     = 501,
-    BAD_GATEWAY                         = 502,
-    SERVICE_UNAVAILABLE                 = 503,
-    GATEWAY_TIMEOUT                     = 504,
-    VERSION_NOT_SUPPORTED               = 505,
-    VARIANT_ALSO_NEGOTIATES             = 506,  -- RFC 2295
+    ['Internal Server Error']               = 500,
+    ['Not Implemented']                     = 501,
+    ['Bad Gateway']                         = 502,
+    ['Service Unavailable']                 = 503,
+    ['Gateway Timeout']                     = 504,
+    ['HTTP Version Not Supported']          = 505,
+    ['Variant Also Negotiates']             = 506,  -- RFC 2295
     -- WebDAV
-    INSUFFICIENT_STORAGE                = 507,  -- WebDAV; RFC 4918
-    LOOP_DETECTED                       = 508,  -- WebDAV; RFC 5842
+    ['Insufficient Storage']                = 507,  -- WebDAV; RFC 4918
+    ['Loop Detected']                       = 508,  -- WebDAV; RFC 5842
     -- /WebDAV
-    BANDWIDTH_LIMIT_EXCEEDED            = 509,  -- Apache bw/limited extension
-    NOT_EXTENDED                        = 510,  -- RFC 2774
-    NETWORK_AUTHENTICATION_REQUIRED     = 511,  -- RFC 6585
+    ['Not Extended']                        = 510,  -- RFC 2774
+    ['Network Authentication Required']     = 511,  -- RFC 6585
 };
-local STATUS_TBL = {};
+local READABLE_TBL = {};
+local STATUS_NAME_TBL = {};
+local STATUS_CODE_TBL = {};
 do
+    local CONVERSION_TBL = { 
+        [' '] = '_', 
+        ['-'] = '_'
+    };
+    
 	for k, v in pairs( HTTP_STATUS ) do
-		STATUS_TBL[tostring(v)] = k;
+        READABLE_TBL[tostring(v)] = k;
+        -- convert space and hyphen to underscore
+        k = k:upper():gsub( '[- ]', CONVERSION_TBL );
+        STATUS_NAME_TBL[k] = v;
+		STATUS_CODE_TBL[tostring(v)] = k;
 	end
 end
 
 -- Class
 local Constants = require('halo').class.Constants;
 
-Constants.property( HTTP_STATUS );
+Constants.property( STATUS_NAME_TBL );
 
 function Constants.toString( val )
-    return STATUS_TBL[tostring(val)];
+    return STATUS_CODE_TBL[tostring(val)];
 end
 
+function Constants.toReadable( val )
+    return READABLE_TBL[tostring(val)];
+end
 
 function Constants.copy( tbl )
-	for _, v in ipairs({ HTTP_STATUS, HTTP_METHOD }) do
+	for _, v in ipairs({ STATUS_NAME_TBL, HTTP_METHOD }) do
 		for k, v in pairs( v ) do
 			tbl[k] = v;
 		end
